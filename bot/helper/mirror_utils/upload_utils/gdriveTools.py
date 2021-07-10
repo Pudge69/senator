4a9cb6245297625578a0f88d9954df755d219bc0
Pudge69 <77266603+Pudge69@users.noreply.github.com>
2021-07-10 15:23:16 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -115,7 +115,7 @@ class GoogleDriveHelper:
                                      resumable=False)
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded using Slam Mirror Bot',
+            'description': 'Uploaded using SenaTorBot',
             'mimeType': mime_type,
         }
         if parent_id is not None:
@@ -169,7 +169,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by Slam Mirror Bot',
+            'description': 'Uploaded by SenaTorBot',
             'mimeType': mime_type,
         }
         try:
@@ -535,8 +535,8 @@ class GoogleDriveHelper:
                     content += f'<b> | <a href="https://telegra.ph/{self.path[nxt_page]}">Next</a></b>'
                     nxt_page += 1
             Telegraph(access_token=telegraph_token).edit_page(path = self.path[prev_page],
-                                 title = 'Slam Mirror Bot Search',
-                                 author_name='Slam Mirror Bot',
+                                 title = 'SenaTorBot Search',
+                                 author_name='SenaTorBot',
                                  author_url='https://github.com/breakdowns/slam-mirrorbot',
                                  html_content=content)
         return
@@ -620,8 +620,8 @@ class GoogleDriveHelper:
 
             for content in self.telegraph_content :
                 self.path.append(Telegraph(access_token=telegraph_token).create_page(
-                                                        title = 'Slam Mirror Bot Search',
-                                                        author_name='Slam Mirror Bot',
+                                                        title = 'SenaTorBot Search',
+                                                        author_name='SenaTorBot',
                                                         author_url='https://github.com/breakdowns/slam-mirrorbot',
                                                         html_content=content
                                                         )['path'])
