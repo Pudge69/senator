1255b6772b13885081bc14f87b053d86ad296f95
Pudge69 <77266603+Pudge69@users.noreply.github.com>
2021-08-18 14:20:22 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -117,7 +117,7 @@ class GoogleDriveHelper:
                                      resumable=False)
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded using Slam Mirrorbot',
+            'description': 'Uploaded using SenTorBot',
             'mimeType': mime_type,
         }
         if parent_id is not None:
@@ -171,7 +171,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by Slam Mirrorbot',
+            'description': 'Uploaded by SenTorBot',
             'mimeType': mime_type,
         }
         try:
@@ -557,9 +557,9 @@ class GoogleDriveHelper:
                     content += f'<b> | <a href="https://telegra.ph/{self.path[nxt_page]}">Next</a></b>'
                     nxt_page += 1
             Telegraph(access_token=telegraph_token).edit_page(path = self.path[prev_page],
-                                 title = 'Slam Mirrorbot Search',
-                                 author_name='Slam Mirrorbot',
-                                 author_url='https://github.com/breakdowns/slam-mirrorbot',
+                                 title = 'SenTorBot Search',
+                                 author_name='SenTorBot',
+                                 author_url='https://t.me/JasonWayneTodd',
                                  html_content=content)
         return
 
@@ -642,9 +642,9 @@ class GoogleDriveHelper:
 
             for content in self.telegraph_content :
                 self.path.append(Telegraph(access_token=telegraph_token).create_page(
-                                                        title = 'Slam Mirrorbot Search',
-                                                        author_name='Slam Mirrorbot',
-                                                        author_url='https://github.com/breakdowns/slam-mirrorbot',
+                                                        title = 'SenTorBot Search',
+                                                        author_name='SenTorBot',
+                                                        author_url='https://t.me/JasonWayneTodd',
                                                         html_content=content
                                                         )['path'])
 
